8635631de06ec973adf5c7c1251a916f2c2b5b83
Ilham <ilham.personal29@gmail.com>
2022-05-24 21:09:37 +0800

    Fix hello world testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,6 @@ const request = require("supertest");
 describe("Get valid response", () => {
   it("return hello world", async () => {
     const response = await request(app).get("/");
-    expect(response.body.message).toBe("Hello World");
+    expect(response.body.message).toBe("Hello World!");
   });
 });
