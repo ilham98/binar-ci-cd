8af9acd6d5ca4c898e83080b4fc9d3459295a82d
Ilham <ilham.personal29@gmail.com>
2022-05-24 20:32:47 +0800

    Initial update

---
+++ __tests__/app.test.js
@@ -0,0 +1,9 @@
+const app = require("../src/app");
+const request = require("supertest");
+
+describe("Get valid response", () => {
+  it("return hello world", async () => {
+    const response = await request(app).get("/");
+    expect(response.body.message).toBe("Hello World");
+  });
+});
